2fccd23f52dd7524eee1666dbe4f85bfd023d5c8
training-dev <contact@training-dev.fr>
2025-10-30 17:53:04 +0100

    soustraction

--- calculatrice.js
+++ calculatrice.js
@@ -2,3 +2,21 @@ const fielda = document.querySelector('#fielda');
 const fieldb = document.querySelector('#fieldb');
 
 
+const resultField = document.querySelector('.result');
+
+
+const substractButton = document.querySelector('#calculateSubstract');
+
+function substract(a,b){
+    return a-b;
+}
+
+function showResult(result){
+    resultField.innerHTML = result;
+}
+
+substractButton.addEventListener('click',function(){
+    let result = substract(parseFloat(fielda.value),parseFloat(fieldb.value));
+    showResult(result);
+
+})
