c73d214be7b0a3a88961b9345d38892afe2e30dc
training-dev <contact@training-dev.fr>
2025-10-30 17:50:48 +0100

    sum

--- calculatrice.js
+++ calculatrice.js
@@ -1,4 +1,20 @@
 const fielda = document.querySelector('#fielda');
 const fieldb = document.querySelector('#fieldb');
+const resultField = document.querySelector('.result');
 
 
+const sumButton = document.querySelector('#calculateSum');
+
+function sum(a,b){
+    return a+b;
+}
+
+function showResult(result){
+    resultField.innerHTML = result;
+}
+
+sumButton.addEventListener('click',function(){
+    let result = sum(parseFloat(fielda.value),parseFloat(fieldb.value));
+    showResult(result);
+
+})
